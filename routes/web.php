67228e16329ca209bf7c732cb4002aac4ce60418
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2020-07-16 14:39:20 -0300

    feat: Adiciona arquivo de rotas

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Route::get('/', 'BookController@show')->name('book.show');
+// Salvar o registro
+Route::post('/', 'BookController@store')->name('book.store');
+
+// Listar o registro
+Route::get('/livro', 'BookController@index')->name('book.index');
+
+// Listar o registro
+Route::put('/livro/{id}', 'BookController@edit')->name('book.edit');
+
+// Deletar o registro
+Route::delete('/livro/{id}', 'BookController@destroy')->name('book.destroy');
+
+
