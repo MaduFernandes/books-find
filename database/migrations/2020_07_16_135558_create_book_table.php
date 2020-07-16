0be86ef20ef83bda463e49b02ee081fe7a4c5625
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2020-07-16 14:42:50 -0300

    feat: Adiciona nova migration

---
+++ database/migrations/2020_07_16_135558_create_book_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('autor');
+            $table->year('ano_de_publicacao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book');
+    }
+}
