9291440f748449100051093ecac8f5720a79803c
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2020-11-30 16:49:14 -0300

    feat: add card book

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -24,6 +24,7 @@
 </head>
 
 <body>
+    <div class="content">
     <nav>
         <a href="#" class="logo">
             <img src="{{url('/logo/logo2.jpg')}}" id="logo" alt="logo">
@@ -42,42 +43,77 @@
         </div>
 
         <ul class="menu">
-            <li><a href="#">Entrar</a></li>
+            <li><a href="{{ route('login') }}">Login</a></li>
         </ul>
     </nav>
 
-        <section id="main">
-            <h1>Livros</h1>
-
-            <div class="show-box">
-                <a href="#">
-                    <img src="{{ url('/livros/livro1.jpeg')}}">
-                </a>
-            </div>
-
-            <br>
-
-            <div class="content">
-                <div class="col-md-6 position-static p-4 pl-md-0">
-                    <form method="POST" action="/">
-                        @csrf
-                            <div class="form-group">
-                                <input class="form" name="nome" placeholder="Livro" class="@error('nome') is-invalid @enderror">
-
-                            </div>
-                            <div class="form-group">
-                                <input class="form" name="autor" placeholder="Autor" class="@error('autor') is-invalid @enderror">
-
-                            </div>
-                            <div class="form-group">
-                                <input class="form" name="ano_de_publicacao" placeholder="Ano de Publicação" class="@error('ano_de_publicacao') is-invalid @enderror">
-
-                            </div>
-                                <input type="submit" value="Salvar" id="btn" class="btn">
-                        </div>
-                    </form>
+    <section id="main">
+        <h1>Livros</h1>
+        @foreach ($book as $list)
+            <div class="card-group">
+                <div class="card">
+                    <img class="img" src="{{ url('/livros/livro1.jpeg')}}" alt="{$list->nome}}">
+                    <div class="card-body">
+                    <h5 class="card-title">{{$list->nome}}</h5>
+                    <p class="card-text">{{$list->autor}}</p>
+                    </div>
                 </div>
             </div>
-        </section>
+        @endforeach
+        <br>
+    </section>
+
+    <div class="content">
+        <div class="col-md-6 position-static p-4 pl-md-0">
+            <form method="POST" action="{{ route('book.search') }}">
+                @csrf
+
+                    <div class="form-group">
+                        <input class="form" name="search" placeholder="Livro" class="@error('nome') is-invalid @enderror">
+                    </div>
+
+                    <div class="form col">
+                        <input type="submit" value="Buscar" id="btn" class="btn">
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+
+        <footer class="page-footer font-small special-color-dark pt-4">
+        <div class="container">
+            <ul class="list-unstyled list-inline text-center">
+            <li class="list-inline-item">
+                <a class="btn-floating btn-fb mx-1">
+                <i class="fab fa-facebook-f"> </i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a class="btn-floating btn-tw mx-1">
+                <i class="fab fa-twitter"> </i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a class="btn-floating btn-gplus mx-1">
+                <i class="fab fa-google-plus-g"> </i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a class="btn-floating btn-li mx-1">
+                <i class="fab fa-linkedin-in"> </i>
+                </a>
+            </li>
+            <li class="list-inline-item">
+                <a class="btn-floating btn-dribbble mx-1">
+                <i class="fab fa-dribbble"> </i>
+                </a>
+            </li>
+            </ul>
+        </div>
+        <div class="footer-copyright text-center py-3" id="copyright">© 2020 Copyright:
+            <a href="https://https://www.unidesc.edu.br/"> Unidesc.edu.br</a>
+        </div>
+        </footer>
+    </div>
 </body>
 </html>
