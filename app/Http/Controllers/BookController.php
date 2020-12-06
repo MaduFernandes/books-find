47616264da38d2df25c1de503c8163380f59de4b
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2020-12-06 12:43:23 -0300

    refactor: remove unused methods in controller

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,53 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreBook;
-use App\Book;
+use App\Models\Book;
 
 class BookController extends Controller
 {
 
+    public function __construct(Request $request, Book $book) {
+        $this->request = $request;
+        $this->repository = $book;
+    }
+
     public function index() {
         $book = Book::all();
-        return view ('listar', compact('book'));
+        return view ('layouts.client.home', compact('book'));
     }
 
+    public function search(Request $request) {
+        $books = $this->repository->search($request->search);
 
-    public function create() {
-        return view('cadastro');
-    }
-
-    public function store(StoreBook $request) {
-
-        $book = new Book;
-        $book->nome = $request->nome;
-        $book->autor = $request->autor;
-        $book->ano_de_publicacao = $request->ano_de_publicacao;
-        $book->save();       
-
-        return redirect()->route('book.index');
-    }
-
-    public function show($id) {
-        $book = Book::findOrFail($id);
-        return view ('show', compact('book'));
-    }
-
-
-    public function edit($id) {
-        $book = Book::findOrFail($id);
-        return view ('edit', compact('book'));
-    }
-
-
-    public function update(Request $request, $id) {
-        $book = Book::find($id);
-        $book->update($request->all());
-        return redirect()->route('book.index');
-        
-    }
-
-    public function destroy($id) {
-        Book::find($id)->delete();
-        return redirect()->route('book.index');
+        return view('layouts.client.home', compact('books'));
     }
 }
