5e6482f5365c320e51153240b428ab3fda802574
MaduFernandes <eduarda.alves.siqueira.fernandes@gmail.com>
2020-12-09 22:30:38 -0300

    feat: add middlewares in routes

--- routes/web.php
+++ routes/web.php
@@ -15,24 +15,15 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-// Listar o registro
-Route::get('/', 'BookController@index')->name('book.index');
+//Login
+Route::view('/', 'auth/login');
 
-// Cadastra o registro
-Route::get('/livro/cadastrar', 'BookController@create')->name('book.create');
-
-// Salvar o registro
-Route::post('/', 'BookController@store')->name('book.store');
-
-Route::get('/livro/{id}/visualizar', 'BookController@show')->name('book.show');
-
-// Editar o registro
-Route::get('/livro/{id}/editar', 'BookController@edit')->name('book.edit');
-
-// Atualizar o registro
-Route::put('/livro/{id}', 'BookController@update')->name('book.update');
-
-// Deletar o registro
-Route::delete('/livro/{id}', 'BookController@destroy')->name('book.destroy');
-
-
+Route::group(['middleware' => 'auth'], function (){
+Route::get('/admin', 'BookController@index')->name('book.index');
+Route::get('/admin/create', 'BookController@create')->name('book.create');
+Route::post('/admin', 'BookController@store')->name('book.store');
+Route::get('/admin/{id}/show', 'BookController@show')->name('book.show');
+Route::get('/admin/{id}/edit', 'BookController@edit')->name('book.edit');
+Route::put('/admin/{id}', 'BookController@update')->name('book.update');
+Route::delete('/admin/{id}', 'BookController@destroy')->name('book.destroy');
+});
